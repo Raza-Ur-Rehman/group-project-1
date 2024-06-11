0e33d700d580ad3afa97a9b220cae620f02d6e84
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-12 00:29:51 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+let searchInput = document.getElementById("search-input");
+let productBox = document.getElementById("productbox");
+
+let productArry = [ `${productBox}`];
+console.log(productArry);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
